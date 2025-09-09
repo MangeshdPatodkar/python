20565398a14b2d2d79f09cd2f9d8afe01b5f925d
MangeshdPatodkar <102413456+MangeshdPatodkar@users.noreply.github.com>
2025-09-09 13:00:05 +0530

    Add files via upload

---
+++ udf2.py
@@ -0,0 +1,10 @@
+def fun():             #define function
+    print("hello!!")
+    print("world")
+
+def fun1():             #define function
+    print("virat")
+    print("kohli")
+    print("india")
+fun()                   #function calling
+fun1()                  #function calling
